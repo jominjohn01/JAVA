0fd8cbd1a1565235502a5cf96d7f8d4c6f658c2f
jomin <142647984+jominjohn01@users.noreply.github.com>
2023-08-22 16:12:12 +0530

    Add files via upload
    java Package

---
+++ Test.java
@@ -0,0 +1,5 @@
+package Jomin;
+public class Test{
+public void display(){
+System.out.println("I am Optimus prime");
+}}
---
+++ Test1.java
@@ -0,0 +1,7 @@
+import Jomin.*;
+import java.util.*;
+class Test1{
+public static void main(String[] args){
+Test aj=new Test();
+aj.display();
+}}
